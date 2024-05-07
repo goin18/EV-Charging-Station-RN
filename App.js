6d9395cf3eb2b790ea97bfc3945ed44013f53ac5
Marko Budal <markobudal@gmail.com>
2024-05-07 12:55:11 +0200

    Add clerk-expo

--- App.js
+++ App.js
@@ -1,15 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
+import * as SecureStore from "expo-secure-store";
 
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { useCallback } from 'react';
 
 import LoginScreen from './App/Screen/LoginScreen/LoginScreen';
 
+import { ClerkProvider,  SignedIn, SignedOut } from "@clerk/clerk-expo";
+
+
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+
+  const tokenCache = {
+    async getToken(key) {
+      try {
+        return SecureStore.getItemAsync(key);
+      } catch (err) {
+        return null;
+      }
+    },
+    async saveToken(key, value) {
+      try {
+        return SecureStore.setItemAsync(key, value);
+      } catch (err) {
+        return;
+      }
+    },
+  };
 
   const [fontsLoaded, fontError] = useFonts({
     'outfit': require('./assets/fonts/Outfit-Regular.ttf'),
@@ -24,9 +44,18 @@ export default function App() {
   }, [fontsLoaded, fontError]);
 
   return (
-    <SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
-      <LoginScreen />
-    </SafeAreaView>
+    <ClerkProvider 
+      tokenCache={tokenCache}
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+      <SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
+        <SignedIn>
+          <Text>You are Signed in</Text>
+        </SignedIn>
+        <SignedOut>
+          <LoginScreen />
+        </SignedOut>
+      </SafeAreaView>
+    </ClerkProvider>
   );
 }
 
--- App/Screen/LoginScreen/LoginScreen.jsx
+++ App/Screen/LoginScreen/LoginScreen.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 import { Text, View, Image, StyleSheet, TouchableOpacity } from 'react-native'
 import Colors from '../../Utils/Colors'
+import * as WebBrowser from "expo-web-browser";
+import { useWarmUpBrowser } from "../../hooks/warmUpBrouser";
+import { useOAuth } from "@clerk/clerk-expo";
 
+WebBrowser.maybeCompleteAuthSession();
 export default function LoginScreen() {
+  useWarmUpBrowser();
+
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+  const onPress = async () => {
+    try {
+      const { createdSessionId, signIn, signUp, setActive } =
+        await startOAuthFlow();
+
+      if (createdSessionId) {
+        setActive({ session: createdSessionId });
+      } else {
+        // Use signIn or signUp for next steps such as MFA
+      }
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  }
+
     return (
       <View style={{ 
         display: 'flex',
@@ -19,9 +42,9 @@ export default function LoginScreen() {
           <Text style={styles.heading}>Your Ultimate EV charging Station Finder App</Text>
           <Text style={styles.desc}>Find EV charing station near you, plane trip and so much more in just one click</Text> 
           <TouchableOpacity style={styles.button}
-            onPress={() => console.log("Login Button Click")}
+            onPress={onPress}
           >
-            <Text style={styles.buttonText}>Login with Google</Text>
+            <Text style={styles.buttonText}>Login with Google </Text>
           </TouchableOpacity>
         </View>
       </View>
