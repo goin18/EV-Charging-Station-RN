62ed4b1403a7d519aaf802a45c528222877b37c1
Marko Budal <markobudal@gmail.com>
2024-05-08 10:41:47 +0200

    expo-location and Maps

--- App.js
+++ App.js
@@ -3,18 +3,48 @@ import * as SplashScreen from 'expo-splash-screen';
 import * as SecureStore from "expo-secure-store";
 
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import { useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import LoginScreen from './App/Screen/LoginScreen/LoginScreen';
 
 import { ClerkProvider,  SignedIn, SignedOut } from "@clerk/clerk-expo";
 import TabNavigation from './App/Navigations/TabNavigation';
 import { NavigationContainer } from '@react-navigation/native';
+import { UserLocationContext } from './App/Context/UserLocationContext'
+
+import * as Location from 'expo-location';
 
 
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== 'granted') {
+        console.log(`Permission to access location was denied`);
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      console.log(location);
+      setLocation(location.coords);
+
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
 
   const tokenCache = {
     async getToken(key) {
@@ -49,17 +79,21 @@ export default function App() {
     <ClerkProvider 
       tokenCache={tokenCache}
       publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
-      <SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
-        <SignedIn>
-          {/* <Text>You are Signed in</Text> */}
-         <NavigationContainer>
-          <TabNavigation />
-         </NavigationContainer>
-        </SignedIn>
-        <SignedOut>
-          <LoginScreen />
-        </SignedOut>
-      </SafeAreaView>
+        <UserLocationContext.Provider
+          value={{ location, setLocation}}
+        >
+          <SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
+            <SignedIn>
+              {/* <Text>You are Signed in</Text> */}
+            <NavigationContainer>
+              <TabNavigation />
+            </NavigationContainer>
+            </SignedIn>
+            <SignedOut>
+              <LoginScreen />
+            </SignedOut>
+          </SafeAreaView>
+      </UserLocationContext.Provider>
     </ClerkProvider>
   );
 }
--- App/Screen/ProfileScreen/ProfileScreen.tsx
+++ App/Screen/ProfileScreen/ProfileScreen.tsx
@@ -1,10 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Button } from 'react-native'
 import React from 'react'
+import {useAuth } from "@clerk/clerk-expo";
 
 export default function ProfileScreen() {
+
+  const SignOut = () => {
+    const { isLoaded,signOut } = useAuth();
+    if (!isLoaded) {
+      return null;
+    }
+    return (
+      <View>
+        <Button
+          title="Sign Out"
+          onPress={() => {
+            signOut();
+          }}
+        />
+      </View>
+    );
+  };
+
   return (
     <View>
       <Text>ProfileScreen</Text>
+      <SignOut />
+      
     </View>
   )
 }
